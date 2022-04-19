6e7a655a3a58a75f3fe6eedfb13191100b76d384
Diana Mayorga <diana101210@gmail.com>
2022-04-18 20:23:13 -0500

    Ejercicio Persona en POO

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Persona persona = new Persona("Diana", 32, 45456465);
+
+        System.out.println("Nombre: "+persona.getNombre() +
+                            " Edad: " + persona.getEdad() +
+                            " Telefono: "+ persona.getTelefono());
+
+
     }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Persona {
+class Persona {
+
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    //Constructor
+    public Persona(String nombre, int edad, int telefono){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.telefono = telefono;
+    }
+
+    //Get y set
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+
+
+
+
+
 }
